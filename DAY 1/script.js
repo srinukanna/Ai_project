0b45b8762f593b0be21cfb8234fbd53520cdf33f
srinukanna <kannasrinivasulu8@gmail.com>
2026-03-12 04:33:24 +0530

    myjs

--- DAY 1/script.js
+++ DAY 1/script.js
@@ -1,3 +1,13 @@
-function formatTime(){
-    
+function formatTime(Date){
+    let hours = Date.getHours();
+    let minutes = Date.getMinutes();
+
+    if(minutes<10){ 
+         minutes= "0" + minutes;
+    }
+
+
+    return hours + ":" + minutes;
+
 }
+console.log(formatTime(new Date()));
